1a265c779c238462e7ab5c55ec87c54940bf67f9
lgaieta <lgaieta.dev@gmail.com>
2023-02-17 12:23:14 -0300

    Added next/font and Inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import './globals.css';
+import { Inter } from '@next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang='en'>
             <head />
-            <body>{children}</body>
+            <body className={inter.className}>{children}</body>
         </html>
     );
 }
